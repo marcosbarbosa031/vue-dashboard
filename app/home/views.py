779ea6686b5607401abb858c670892c5bcfc8c69
Marcos Barbosa <enos_mm@hotmail.com>
2018-03-09 14:45:56 -0300

    merge

--- app/home/views.py
+++ app/home/views.py
@@ -51,5 +51,29 @@ def update_boleto():
                           valor_brl, valor_moeda, moeda, hora)
     return jsonify(response)
 
-
+@home.route('/updatecard', methods=['POST'])
+def update_card():
+    if not request.json:
+        return abort(500)
+    card_id = request.json.get('id')
+    nome_cartao = request.json.get('nome_cartao')
+    n_transacao = request.json.get('n_transacao')
+    tipo_cartao = request.json.get('tipo_cartao')
+    n_cartao = request.json.get('n_cartao')
+    n_order = request.json.get('n_order')
+    valor_brl = request.json.get('valor_brl')
+    valor_usd = request.json.get('valor_usd')
+    hora = request.json.get('hora')
+    data = request.json.get('data')
+    status_id = request.json.get('status_id')
+    status = request.json.get('status')
+    motivo = request.json.get('motivo')
+    currency = request.json.get('currency')
+    email = request.json.get('email')
+    data_inicio = request.json.get('data_inicio')
+    data_fim = request.json.get('data_fim')
+    tipo_pag = request.json.get('tipo_pag')
+    card = Card.get_cartao(card_id)
+    response = card.update(nome_cartao, n_transacao, tipo_cartao, n_cartao, n_order, valor_brl, valor_usd, hora, data, status_id, status, motivo, currency, email, data_inicio, data_fim, tipo_pag)
+    return jsonify(response)
 
